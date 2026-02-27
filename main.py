0b2a301c3b3a747c88084777410b5e19f6d9234e
Dagnis-Rainers-Remesis-241RDB183 <dagnis-rainers.remesis@edu.rtu.lv>
2026-02-27 13:26:25 +0200

    Update structure

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import asyncio
 
 from pyray import *
 
+from src.game import Game
+
 
 async def main():
+    game = Game()
+
     init_window(800, 450, "Hello")
     while not window_should_close():
         begin_drawing()
         clear_background(WHITE)
         draw_text("Hello world", 190, 200, 20, VIOLET)
+        draw_text(str(game.value), 190, 240, 20, RED)
         end_drawing()
         await asyncio.sleep(0)
     close_window()
