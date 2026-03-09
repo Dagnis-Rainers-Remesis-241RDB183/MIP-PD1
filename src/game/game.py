f510e237a9c0854ed29a67944e0ac816ad127e9f
Dagnis-Rainers-Remesis-241RDB183 <dagnis-rainers.remesis@edu.rtu.lv>
2026-03-09 23:46:16 +0200

    Update gui

--- src/game/game.py
+++ src/game/game.py
@@ -1,17 +1,80 @@
 import random
+from enum import Enum
 
 
 class Game:
+    class State(Enum):
+        START = 0
+        ACTIVE = 1
+        COMPLETE = 2
+
+    computer_score: int
     numbers: list[int]
+    player_score: int
+    player_start: bool
     selection: int
-    playerScore: int
-    computerScore: int
+    state: State
+    turn: int
 
     def __init__(self) -> None:
-        self.numbers = self._generate_numbers(25)
+        self.computer_score = 0
+        self.numbers = []
+        self.player_score = 0
+        self.player_start = False
         self.selection = 0
-        self.playerScore = 0
-        self.computerScore = 0
+        self.state = self.State.START
+        self.turn = 0
+
+    def move_left(self) -> None:
+        self.selection = max(0, self.selection - 2)
+
+    def move_right(self) -> None:
+        self.selection = min(
+            len(self.numbers) - 2 + len(self.numbers) % 2, self.selection + 2
+        )
+
+    def select(self) -> None:
+        self.turn += 1
+        a = self.numbers.pop(self.selection)
+
+        if len(self.numbers) == self.selection:
+            self._remove_point()
+
+            self.selection = min(
+                len(self.numbers) - 2 + len(self.numbers) % 2, self.selection
+            )
+            return
+
+        b = self.numbers.pop(self.selection)
+        self._replace(a, b)
+
+        self.selection = min(
+            len(self.numbers) - 2 + len(self.numbers) % 2, self.selection
+        )
+
+    def start(self, number_count: int, player_start: bool) -> None:
+        self.computer_score = 0
+        self.numbers = self._generate_numbers(number_count)
+        self.player_score = 0
+        self.player_start = player_start
+        self.selection = 0
+        self.state = self.State.ACTIVE
+        self.turn = 0
+
+    def _add_point(self) -> None:
+        match self.turn % 2:
+            case 0:
+                if self.player_start:
+                    self.computer_score += 1
+                else:
+                    self.player_score += 1
+            case 1:
+                if self.player_start:
+                    self.player_score += 1
+                else:
+                    self.computer_score += 1
+            case _:
+                pass
 
     def _generate_numbers(self, count: int) -> list[int]:
         numbers: list[int] = []
@@ -22,13 +85,26 @@ class Game:
 
         return numbers
 
-    def move_left(self) -> None:
-        self.selection = max(0, self.selection - 1)
+    def _remove_point(self) -> None:
+        match self.turn % 2:
+            case 0:
+                if self.player_start:
+                    self.player_score -= 1
+                else:
+                    self.computer_score -= 1
+            case 1:
+                if self.player_start:
+                    self.computer_score -= 1
+                else:
+                    self.player_score -= 1
+            case _:
+                pass
 
-    def move_right(self) -> None:
-        self.selection = min(len(self.numbers) - 2, self.selection + 1)
+    def _replace(self, a: int, b: int) -> None:
+        sum = a + b
 
-    def select(self) -> None:
-        _a = self.numbers.pop(self.selection)
-        _b = self.numbers.pop(self.selection)
-        self.selection = min(len(self.numbers) - 2, self.selection)
+        if sum > 6:
+            sum -= 6
+
+        self.numbers.insert(self.selection, sum)
+        self._add_point()
---
+++ src/gui/style.py
@@ -0,0 +1,30 @@
+from pyray import *
+from typing_extensions import final
+
+
+@final
+class Style:
+    def __init__(self) -> None:
+        gui_load_style("assets/theme/style_dark.rgs")
+        gui_set_style(0, 16, gui_get_font().baseSize * 2)
+
+        gui_set_style(
+            GuiControl.DEFAULT,
+            GuiDefaultProperty.BACKGROUND_COLOR,
+            gui_get_style(GuiControl.DEFAULT, GuiControlProperty.BASE_COLOR_PRESSED),
+        )
+
+        self.BACKGROUND_COLOR = get_color(
+            gui_get_style(GuiControl.DEFAULT, GuiDefaultProperty.BACKGROUND_COLOR)
+            & 0xFFFFFFFF
+        )
+
+        self.TEXT_COLOR_NORMAL = get_color(
+            gui_get_style(GuiControl.DEFAULT, GuiControlProperty.TEXT_COLOR_NORMAL)
+            & 0xFFFFFFFF
+        )
+
+        self.TEXT_COLOR_PRESSED = get_color(
+            gui_get_style(GuiControl.DEFAULT, GuiControlProperty.TEXT_COLOR_PRESSED)
+            & 0xFFFFFFFF
+        )
